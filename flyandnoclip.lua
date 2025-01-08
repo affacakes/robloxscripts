fa5e10f2886f05dd7173a91197a85306e3de059f
affacakes <107773099+affacakes@users.noreply.github.com>
2025-01-08 13:10:55 +0000

    Key is back to Q

--- flyandnoclip.lua
+++ flyandnoclip.lua
@@ -67,7 +67,7 @@ end
 local UserInputService = game:GetService("UserInputService")
 UserInputService.InputBegan:Connect(function(input)
     if input.UserInputType == Enum.UserInputType.Keyboard then
-        if input.KeyCode == Enum.KeyCode.V then
+        if input.KeyCode == Enum.KeyCode.Q then
             flying = not flying
             if flying then
                 humanoid.PlatformStand = true
